6d342356b78d75df899914d7ddee4f60a290883d
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 13:32:24 +0700

    Music player UI

---
+++ SimpleMusicPlayerUI/SimpleMusicPlayerUI/SimpleMusicPlayerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMusicPlayerUIApp.swift
+//  SimpleMusicPlayerUI
+//
+//  Created by Adrian Suryo Abiyoga on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMusicPlayerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
